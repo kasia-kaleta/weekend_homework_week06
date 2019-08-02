f788e16c4643b2fc8ced64dda08ca1c73af0f8b0
kat3005 <kasia.alicja@gmail.com>
2019-08-02 20:16:25 +0100

    adds event listener

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newCelebForm = document.querySelector('#new-celeb-form');
+  newCelebForm.addEventListener('submit', handleNewCelebFormSubmit);
+)};
+
+  
